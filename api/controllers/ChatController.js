c5efb04d26e6021afa4c0e72398aa1506308a556
urbanrotnik <urby007@gmail.com>
2015-12-24 11:49:35 +0100

    serverside logic

---
+++ api/controllers/ChatController.js
@@ -0,0 +1,30 @@
+/**
+ * ChatController
+ *
+ * @description :: Server-side logic for managing chats
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+  addConv:function (req,res) {
+
+    var data_from_client = req.params.all();
+
+    if(req.isSocket && req.method === 'POST'){
+
+      // This is the message from connected client
+      // So add new conversation
+      Chat.create(data_from_client)
+        .exec(function(error,data_from_client){
+          console.log(data_from_client);
+          Chat.publishCreate({id: data_from_client.id, message : data_from_client.message , user:data_from_client.user});
+        });
+    }
+    else if(req.isSocket){
+      // subscribe client to model changes
+      Chat.watch(req.socket);
+      console.log( 'User subscribed to ' + req.socket.id );
+    }
+  }
+};
